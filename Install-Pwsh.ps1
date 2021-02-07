80b1f0e1e0a0f1cc86aebc15c32d98b73935cfeb
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-02-07 08:25:44 +0100

    Create Install-Pwsh.ps1

---
+++ Install-Pwsh.ps1
@@ -0,0 +1,6 @@
+#requires -runasadministrator 
+
+# DOWNLOAD AND INSTALL THE LATEST VERSION OF POWERSHELL
+# BY USING THE MSI INSTALLER IN UNATTENDED/QUIET MODE.
+
+Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -quiet"
