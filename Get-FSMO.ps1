f2acd8fa8e2255d8d7d3f2089e9b7d66b46faa34
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-02-07 08:39:00 +0100

    Create Get-FSMO.ps1

---
+++ Get-FSMO.ps1
@@ -0,0 +1,7 @@
+# https://blogs.technet.microsoft.com/heyscriptingguy/2014/11/28/powertip-use-powershell-to-get-list-of-fsmo-role-holders/
+Get-ADDomain | Select-Object InfrastructureMaster, RIDMaster, PDCEmulator
+Get-ADForest | Select-Object DomainNamingMaster, SchemaMaster
+Get-ADDomainController -Filter * |
+Select-Object Name, Domain, Forest, OperationMasterRoles |
+Where-Object {$_.OperationMasterRoles} |
+Format-Table -AutoSize
