3d8b8528ee3b987d61dbe48cb319dcc64eb13933
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-02-07 08:26:49 +0100

    Create Install-Chocolatey.ps1

---
+++ Install-Chocolatey.ps1
@@ -0,0 +1,4 @@
+#Requires -RunAsAdministrator
+
+#https://chocolatey.org/docs/installation
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
