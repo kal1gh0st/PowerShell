999cdc85dd95660d003e428d3e1560cb71139889
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-02-09 07:44:09 +0100

    Create Decode-SecureStringPassword.ps1

---
+++ Decode-SecureStringPassword.ps1
@@ -0,0 +1,33 @@
+<#
+.Synopsis
+   Tries to decode a SecureString and returns its clear text value
+.DESCRIPTION
+   Tries to decode a SecureString from a PSCredential Object and returns its clear text value
+.EXAMPLE
+   get-credential | Decode-SecureStringPassword
+.EXAMPLE
+   get-credential | dssp
+.EXAMPLE
+   Decode-SecureStringPassword -password SecureString
+.Notes
+
+#>
+function Decode-SecureStringPassword {
+    [CmdletBinding()]
+    [Alias('dssp')]
+    [OutputType([string])]
+    Param
+    (
+        [Parameter(Mandatory = $true,
+        ValueFromPipelineByPropertyName = $true,
+        Position = 0) ]
+        [SecureString] $password
+    )
+    Begin {
+    }
+    Process {
+        return [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($password))
+    }
+    End {
+    }
+}
