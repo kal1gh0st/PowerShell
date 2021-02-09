0cb3920051f13b26bf1ea49e562d68ffb64ab931
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-02-09 07:39:04 +0100

    Create Disable-WindowsFirewall.ps1

---
+++ Disable-WindowsFirewall.ps1
@@ -0,0 +1,8 @@
+#requires -runasadministrator
+
+#
+# This script will disable the windows firewall for all profiles PUBLIC, DOMAIN, PRIVATE
+#
+Set-NetFirewallProfile -profile Public,domain,private -enabled false
+# To check the FIREWALL PROFILE STATUS use
+# Get-NetFirewallProfile | select-object name, enabled
